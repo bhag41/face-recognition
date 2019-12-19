a190c5953a64ce10bb02255936a30f5d3c52613e
Bhagyashree <bhag41@gmail.com>
2019-12-19 15:57:40 -0800

    colors changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
-import Signin from './components/Signin/Signin';
+import Signin from './components/SignIn/Signin';
 import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
